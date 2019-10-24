2aa05557ff9666abb07080fb91fcf43312d34593
Ben <johndoe@example.com>
2019-10-24 14:16:39 +0200

    initial commit

---
+++ Taches.java
@@ -0,0 +1,19 @@
+public class Taches{
+
+    //Déclarations des attributs
+    String nom;
+	String description;
+	boolean etat;
+	//Collection<String> historique;
+
+    public Taches(){
+        //Initialisation des attributs
+        nom = "";
+        description = "";
+        etat = false;
+        //historique = null;
+    }
+
+    //Cette méthode permet d'ajouter
+
+}
