b6a14bf30988db30ee82abfa90c0206a7aa57de7
Ben <johndoe@example.com>
2019-10-24 15:15:15 +0200

    add:(choix 1 et 4) fonctionnel - ajout tache & lister taches

--- Main.java
+++ Main.java
@@ -13,66 +13,11 @@ public class Main{
         //Je créé ma liste
         ToDo maListe = new ToDo(recupNomToDoList);
 
-        //Je renvoi une ligne pour mieux voir
-        System.out.println("-------------------------------------------");
-
-        //Je vais demander à l'utilisateur de mettre une tache
-        //Cette variable me permet de boucler, si la personne veut remettre une tache 
-        //par la suite ( en continu )
-        boolean ajoutTache = false;
-
-        //Tant que #ajoutTache n'est pas vrai, l'utilisateur entre une tache
-        while(ajoutTache == false){
-
-            System.out.println("---------------- NOUVELLE TACHE -----------------");
-            //Je dois remettre a zero mon scanner
-            //Pour corriger le bug du eux .nextLine des que passe la deuxieme iteration
-            sc = new Scanner(System.in);
-            //Je demande le nom de la tache
-            System.out.println("Entrez le nom de votre tache :");
-            //Je recupere le nom de la tache
-            //sc.nextLine();
-            String recupNomTache = sc.nextLine();
-            
-            //Je demande la description de la tache
-            System.out.println("Entrez la description de votre tache :");
-            //Je recupere la description de la tache
-            String recupDescTache = sc.nextLine();
-
-            //Je demande l"état de la tache
-            System.out.println("Entrez l'état de votre tache (1 = finis, 0 = a faire) :");
-
-            //Cette variable me permet de convertir le 1 et 0
-            boolean recupEtatTache;
-            //Si je recupere 1 alors je passe a true mon etat
-            if(sc.nextInt() == 1){
-                recupEtatTache = true;
-            }else{
-                //Sinon je passe a false
-                recupEtatTache = false;
-            }
-            
-            //J'ajoute la tache (création de ma tache)
-            Taches tacheAjout = new Taches(recupNomTache, recupDescTache, recupEtatTache);
-
-            //J'ajoute a la tache a ma liste
-            maListe.addTaches(tacheAjout);
-
-            //Lorsque j'ai ajouté la tache
-            //Je vais demander à l'utilisateur si il veut continuer ou pas
-            System.out.println("Voulez-vous ajouter une tache (1 = oui, 0 = non)");
-
-            if(sc.nextInt() == 0){
-                ajoutTache = true;
-            }
-            
-        }
-
         //Création du menu
         boolean arretMenu = false;
         while (!arretMenu){
             System.out.println("--------------- MENU ----------------");
-            System.out.println("1. Ajouter une tache");
+            System.out.println("1. Ajouter une/des tache(s)");
             System.out.println("2. Modifier une tache");
             System.out.println("3. Supprimer une tache");
             System.out.println("4. Afficher les taches");
@@ -80,7 +25,57 @@ public class Main{
 
             switch(sc.nextInt()){
                 case 1 : 
-                    //System.out.println("Sous menu 1-1"); 
+                    //Je vais demander à l'utilisateur de mettre une tache
+                    //Cette variable me permet de boucler, si la personne veut remettre une tache 
+                    //par la suite ( en continu )
+                    boolean ajoutTache = false;
+
+                    //Tant que #ajoutTache n'est pas vrai, l'utilisateur entre une tache
+                    while(ajoutTache == false){
+
+                        System.out.println("---------------- NOUVELLE TACHE -----------------");
+                        //Je dois remettre a zero mon scanner
+                        //Pour corriger le bug du eux .nextLine des que passe la deuxieme iteration
+                        sc = new Scanner(System.in);
+                        //Je demande le nom de la tache
+                        System.out.println("Entrez le nom de votre tache :");
+                        //Je recupere le nom de la tache
+                        //sc.nextLine();
+                        String recupNomTache = sc.nextLine();
+                        
+                        //Je demande la description de la tache
+                        System.out.println("Entrez la description de votre tache :");
+                        //Je recupere la description de la tache
+                        String recupDescTache = sc.nextLine();
+
+                        //Je demande l"état de la tache
+                        System.out.println("Entrez l'état de votre tache (1 = finis, 0 = a faire) :");
+
+                        //Cette variable me permet de convertir le 1 et 0
+                        boolean recupEtatTache;
+                        //Si je recupere 1 alors je passe a true mon etat
+                        if(sc.nextInt() == 1){
+                            recupEtatTache = true;
+                        }else{
+                            //Sinon je passe a false
+                            recupEtatTache = false;
+                        }
+                        
+                        //J'ajoute la tache (création de ma tache)
+                        Taches tacheAjout = new Taches(recupNomTache, recupDescTache, recupEtatTache);
+
+                        //J'ajoute a la tache a ma liste
+                        maListe.addTaches(tacheAjout);
+
+                        //Lorsque j'ai ajouté la tache
+                        //Je vais demander à l'utilisateur si il veut continuer ou pas
+                        System.out.println("Voulez-vous ajouter une tache (1 = oui, 0 = non)");
+
+                        if(sc.nextInt() == 0){
+                            ajoutTache = true;
+                        }
+                        
+                    }
                     break;
                 case 2 : 
                     //System.out.println("Sous menu 1-2"); 
